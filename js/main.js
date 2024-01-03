b345500da1ee570d874f15aebaec22fddb10ee7a
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-01-03 08:43:53 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// insertNavbar.js
+fetch('html/nav-bar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error al cargar la barra de navegación:', error));
+
+fetch('html/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error al cargar el footer:', error));
