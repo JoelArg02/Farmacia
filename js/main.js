60ebe07839f6ef8fa085ebd8b7fa5aa9116103d8
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-01-03 18:47:29 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-// insertNavbar.js
-fetch('html/nav-bar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar la barra de navegación:', error));
+let productos = [];
 
-fetch('html/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el footer:', error));
+fetch("html/nav-bar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
+  })
+  .catch((error) =>
+    console.error("Error al cargar la barra de navegación:", error)
+  );
+
+fetch("html/content.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("content-placeholder").innerHTML = data;
+  })
+  .catch((error) => console.error("Error al cargar el content:", error));
+
+fetch("html/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer-placeholder").innerHTML = data;
+  })
+  .catch((error) => console.error("Error al cargar el footer:", error));
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  cargarProductos();
+  actualizarCategorias();
+});
